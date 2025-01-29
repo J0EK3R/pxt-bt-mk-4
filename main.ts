2dff563a50f4dd5c6473c068f7b9242b0b589b54
J0EK3R <J0EK3R@gmx.net>
2025-01-29 10:51:25 +0100

    JK: test

--- main.ts
+++ main.ts
@@ -2,9 +2,31 @@
 /**
  * Support for M0uld K1ng Module 4.0.
  */
-//% color=#0c3000 weight=100 icon="\uf294" block="MK Module 4.0"
-namespace MK
+//% color=#00c300 weight=100 icon="\uf294" block="MK Module 4.0"
+namespace MK4
 {
+    export enum Channel
+    {
+        //% block="Channel A" enumval=0
+        A = 0,
+        //% block="Channel B" enumval=1
+        B = 1,
+        //% block="Channel C" enumval=2
+        C = 2,
+        //% block="Channel D" enumval=3
+        D = 3,
+    }
+
+    export enum Module
+    {
+        //% block="Module 1" enumval=0
+        M1 = 0,
+        //% block="Module 2" enumval=1
+        M2 = 1,
+        //% block="Module 3" enumval=2
+        M3 = 2,
+    }
+
     /**
     *  M0uld K1ng Module 4.0 init
     * @param moduleNo moduleNo, eg: "Module 1"
